70d712c3a5d29735a8d148085e2ad969526bb408
Kuzma <kuzmakulisin@macbook-pro-kuzma.local>
2019-01-13 17:33:36 +0300

    Initial Commit

---
+++ HW 3.8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 3.8
+//
+//  Created by Kuzma on 13/01/2019.
+//  Copyright © 2019 Kuzma Kulishin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
